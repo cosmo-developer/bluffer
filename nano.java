9a43a2579af6dc68e96e3412783e6aca5898e635
Sonu Aryan <sonuaryan76448002@gmail.com>
2022-01-01 20:08:34 +0530

    changes done

---
+++ nano.java
@@ -0,0 +1,11 @@
+public class nano{
+	public static void main(String[] args){
+		
+	}
+	public static float gener(int x,int y){
+		int z=x+y;
+		int fix;
+		String jenifier="Aesop";
+		return 2.3f;
+	}
+}
--- src/com/bluff/compiler/phases/ByteCodeGenerator.java
+++ src/com/bluff/compiler/phases/ByteCodeGenerator.java
@@ -5,6 +5,7 @@
  */
 package com.bluff.compiler.phases;
 
+import com.bluff.SymbolTable;
 import com.bluff.expr.AddExpression;
 import com.bluff.expr.ArgumentExpression;
 import com.bluff.expr.AssignmentExpression;
@@ -20,20 +21,47 @@ import com.bluff.expr.EquExpression;
 import com.bluff.expr.Expression;
 import com.bluff.expr.FactorExpression;
 import com.bluff.expr.IfExpression;
+import com.bluff.expr.IntegeralLiteralExpression;
 import com.bluff.expr.MethodCallExpression;
 import com.bluff.expr.MethodDeclarationExpression;
 import com.bluff.expr.ParameterExpression;
 import com.bluff.expr.RelExpression;
 import com.bluff.expr.StatementExpression;
 import com.bluff.expr.StatementListExpression;
+import com.bluff.expr.StringLiteralExpression;
 import com.bluff.expr.TermExpression;
 import com.bluff.expr.WhileExpression;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.objectweb.asm.ClassWriter;
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.objectweb.asm.Opcodes.ACC_STATIC;
+import static org.objectweb.asm.Opcodes.V1_8;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+import org.objectweb.asm.commons.Method;
 
 /**
  *
  * @author Sonu Aryan <cosmo-developer@github.com>
  */
 public class ByteCodeGenerator {
+    GeneratorAdapter currentAdapter;
+    ClassWriter cw;
+    SymbolTable gtable;
+    final int GEN_FLAG=ACC_PUBLIC+ACC_STATIC;
+    final String className;
+    public ByteCodeGenerator(String className, SymbolTable table) {
+        cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        this.gtable = table;
+        cw.visit(V1_8, ACC_PUBLIC, className, null, "java/lang/Object", null);
+        this.className=className;
+    }
 
     public Object visit(Expression exp) {
         if (exp instanceof AddExpression) {
@@ -96,11 +124,29 @@ public class ByteCodeGenerator {
     }
 
     public Object visit(BlockExpression aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        aThis.statements.forEach((e) -> {
+            this.visit(e);
+        });
+        return null;
     }
 
     public Object visit(DeclVariableInitializedExpression aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        SymbolTable.Symbol symbol = gtable.getSymbol(aThis.identifier);
+        Object visit = this.visit(aThis.initialize);
+        if (visit==null){
+            if (aThis.initialize instanceof StringLiteralExpression){
+                StringLiteralExpression sle=(StringLiteralExpression) aThis.initialize;
+                this.currentAdapter.newLocal(Type.getType(String.class));
+                this.currentAdapter.push(sle.value);
+                this.currentAdapter.storeLocal(symbol.lidx+1, Type.getType(String.class));
+            }else if(aThis.initialize instanceof IntegeralLiteralExpression){
+                IntegeralLiteralExpression ile=(IntegeralLiteralExpression) aThis.initialize;
+                this.currentAdapter.newLocal(Type.INT_TYPE);
+                this.currentAdapter.push(ile.value);
+                this.currentAdapter.storeLocal(symbol.lidx+1,Type.INT_TYPE);
+            }
+        }
+        return null;
     }
 
     public Object visit(DeclareAndCreateArrayConstantExpression aThis) {
@@ -156,7 +202,21 @@ public class ByteCodeGenerator {
     }
 
     public Object visit(StatementListExpression aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        aThis.statements.forEach((mde) -> {
+            this.visit(mde);
+        });
+        cw.visitEnd();
+        try {
+            try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(className+".class"))) {
+                bos.write(cw.toByteArray());
+                bos.flush();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ByteCodeGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ByteCodeGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     public Object visit(TermExpression aThis) {
@@ -168,7 +228,36 @@ public class ByteCodeGenerator {
     }
 
     public Object visit(MethodDeclarationExpression aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        SymbolTable.Symbol symbol = gtable.getSymbol(aThis.identifier);
+        if (symbol.id.getText().equals("main") && symbol.type.equals("(void)")) {
+            //Main Function Generator
+            Method m=Method.getMethod("void main (String[])");
+            this.currentAdapter=new GeneratorAdapter(GEN_FLAG,m,null,null,cw);
+            this.gtable=aThis.table;
+            this.visit((BlockExpression)aThis.body);
+            
+            this.gtable=this.gtable.parent;
+            this.currentAdapter.returnValue();
+            this.currentAdapter.endMethod();
+        }else{
+            String[] split = symbol.type.split(";");
+            String argtypes="";
+            for (int i=0;i<split.length-1;i++){
+                argtypes+=split[i];
+                if (i<split.length-2)
+                   argtypes+=",";
+            }
+            argtypes="("+argtypes+")";
+            argtypes=split[split.length-1].replace("(", "").replace(")", "")+" "+symbol.id.getText()+" "+argtypes;
+            Method m=Method.getMethod(argtypes);
+            this.currentAdapter=new GeneratorAdapter(GEN_FLAG,m,null,null,cw);
+            this.gtable=aThis.table;
+            this.visit((BlockExpression)aThis.body);
+            this.gtable=this.gtable.parent;
+            this.currentAdapter.returnValue();
+            this.currentAdapter.endMethod();
+        }
+        return null;
     }
 
 }
